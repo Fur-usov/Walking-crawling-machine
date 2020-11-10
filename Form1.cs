2a9de7a0b7e36bcec62ed03d3da544cb2d7a03b1
BufficBoy104 <58601146+BufficBoy104@users.noreply.github.com>
2020-11-10 18:08:02 +0300

    All files

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Project_Kulisa_Visual
+{
+    public partial class Form1 : Form
+    {
+        const double PI = 3.14159265359;
+        const double rad = PI / 180;
+        int R = 3;
+        int L = 5;
+        int E = 8;
+        int period=11;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static double Position_X(int R, int L, int E, double F)
+        {
+            double BC = Math.Sqrt(Math.Pow(E, 2) + Math.Pow(R, 2) - 2 * E * R * Math.Cos(PI / 2 + F)); // добавить квадрат BC
+            double B = Math.Atan(R * Math.Cos(F) / E + R * Math.Sin(F));
+            return Math.Cos(B) * (BC + L);
+        }
+
+        static double Position_Y(int R, int L, int E, double F)
+        {
+            double BC = Math.Sqrt(Math.Pow(E, 2) + Math.Pow(R, 2) - 2 * E * R * Math.Cos(PI / 2 + F)); // добавить квадрат BC
+            double B = Math.Atan(R * Math.Cos(F) / E + R * Math.Sin(F));
+            return Math.Sin(B) * (BC + L);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+           
+            Graphics g = e.Graphics;
+            //g.TranslateTransform(f.Height/2, f.Width/2 );
+            
+            g.DrawRectangle(new Pen(Color.Blue), 100, 100, 200, 200);
+            for (double i = 0.0; i <= 2 * PI; i += period * rad)
+            {
+                g.DrawLine(new Pen(Color.Black), (int)Position_X(R, L, E, i) , (int )Position_Y(R, L, E, i), (int)Position_X(R, L, E, i), (int)Position_Y(R, L, E, i));
+            }
+        }
+    }
+}
