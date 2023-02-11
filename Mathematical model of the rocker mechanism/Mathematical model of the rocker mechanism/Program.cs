57584ebf8138d3bcdf87dbd408c37399eaaf23c9
Fedor <fsurusov@miem.hse.ru>
2023-02-11 11:40:01 +0300

    Cleaned up all project
    Current version didn't work. I left only code which has sense

---
+++ Mathematical model of the rocker mechanism/Mathematical model of the rocker mechanism/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mathematical_model_of_the_rocker_mechanism
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
